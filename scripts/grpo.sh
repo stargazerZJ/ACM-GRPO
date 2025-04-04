1301af4e5b62cda2501e0537d1478cdda0421e0e
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2025-04-04 20:53:53 +0800

    feat: optimize bash scripts

--- scripts/grpo.sh
+++ scripts/grpo.sh
@@ -1,12 +1,19 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
+
+# checkpoints, wandb, and other output files will be stored alongside the project directory
+PROJECT_PARENT=$(dirname "$PROJECT_DIR")
+
 export HOST_IP=$MASTER_ADDR
 export NCCL_CUMEM_ENABLE=0
 export TOKENIZERS_PARALLELISM=False
 
 
-# wandb setting
-export WANDB_MODE=offline or online
-export WANDB_DIR=
-pkill -f ray
+# wandb settings
+export WANDB_MODE=online
+export WANDB_DIR=$PROJECT_PARENT
+ray stop
 
 
 ROLLOUT_BS=64
@@ -21,32 +28,32 @@ EVAL_STEPS=10
 MAX_GEN_LEN=1024
 
 TRIAL_NAME=grpo_baseline
+DATE_SUFFIX=$(date +"%Y%m%d_%H%M%S")
 
-DATA_PATH=./data/train/math3k_rl_prompt
+DATA_PATH=$PROJECT_DIR/data/train/math3k_rl_prompt
 # model path
 POLICY_MODEL_PATH=Qwen/Qwen2.5-Math-1.5B
 
 
-SAVE_PATH=../ckpts/${TRIAL_NAME}
-SAMPLES_SAVE_PATH=./data/output/rl/${TRIAL_NAME}
+SAVE_PATH=$PROJECT_PARENT/ckpts/${TRIAL_NAME}_${DATE_SUFFIX}
+SAMPLES_SAVE_PATH=$PROJECT_PARENT/output/${TRIAL_NAME}_${DATE_SUFFIX}
 
 # start rm
+RM_PORT=5001
 python -m src.cli.serve_rm \
     --mode rule \
     --tokenizer_path $POLICY_MODEL_PATH \
     --max_gen_len $MAX_GEN_LEN \
     --data_path $DATA_PATH \
-    --port 5000 &
+    --port $RM_PORT &
 
-sleep 10s
 RAY_MASTER_PORT=6379
 RAY_DASHBOARD_PORT=8265
 ray start --head  --port=$RAY_MASTER_PORT --dashboard-host=127.0.0.1 --dashboard-port=$RAY_DASHBOARD_PORT --num-gpus 4
 
-sleep 10s
 # replace working_dir with your own working dir
 RAY_ADDRESS="http://127.0.0.1:$RAY_DASHBOARD_PORT" ray job submit --address="http://127.0.0.1:12345" \
-    --runtime-env-json='{"working_dir": "/inspire/hdd/ws-950e6aa1-e29e-4266-bd8a-942fc09bb560/embodied-intelligence/liupengfei-24025/xfli/o1/reference/cs2916/homework1"}' \
+    --runtime-env-json="{\"working_dir\": \"$PROJECT_PARENT/tmp\"}" \
     -- python3 -m src.cli.train_ppo_ray \
     --ref_num_nodes 1 \
     --ref_num_gpus_per_node 1 \
@@ -58,7 +65,7 @@ RAY_ADDRESS="http://127.0.0.1:$RAY_DASHBOARD_PORT" ray job submit --address="htt
     --save_steps 10 \
     --pretrain $POLICY_MODEL_PATH \
     --ref_pretrain $POLICY_MODEL_PATH \
-    --remote_rm_url http://localhost:5000/get_reward \
+    --remote_rm_url http://localhost:$RM_PORT/get_reward \
     --save_path $SAVE_PATH \
     --ckpt_path $SAVE_PATH \
     --samples_save_path $SAMPLES_SAVE_PATH \
@@ -78,7 +85,7 @@ RAY_ADDRESS="http://127.0.0.1:$RAY_DASHBOARD_PORT" ray job submit --address="htt
     --lr_warmup_steps 10 \
     --init_kl_coef $KL_COEF \
     --prompt_data $DATA_PATH \
-    --test_path ./data/eval/RL.jsonl \
+    --test_path $PROJECT_DIR/data/eval/RL.jsonl \
     --input_key context_messages \
     --apply_chat_template \
     --max_samples 100000 \
@@ -89,8 +96,8 @@ RAY_ADDRESS="http://127.0.0.1:$RAY_DASHBOARD_PORT" ray job submit --address="htt
     --vllm_sync_backend nccl \
     --gradient_checkpointing \
     --temperature $TEMPERATURE \
-    --use_wandb "WANDB TOKEN" \
-    --wandb_project "WANDB PROJECT" \
+    --use_wandb "$WANDB_API_KEY" \
+    --wandb_project "sjtu-cs2916-grpo" \
     --wandb_group rl.grpo \
     --wandb_run_name $TRIAL_NAME &
-wait   
+wait
